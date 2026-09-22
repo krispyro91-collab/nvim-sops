156b524539654fe96244628471cde6ac8e48c65f
krispyro91-collab <krispyro91@gmail.com>
2026-09-22 23:51:56 +0200

    Simplify file status command construction
    Refactor file status retrieval to simplify arguments.

--- lua/sops.lua
+++ lua/sops.lua
@@ -35,21 +35,16 @@ function M.is_encrypted()
 				seen[marker] = true
 
 				if vim.tbl_count(seen) == #sops_markers then
-					local file_status_args = { "sops", "filestatus" }
-					
-					if not path:match("%.[^/]+$") then
-					    vim.list_extend(file_status_args, {
-					        "--input-type", "json",
-					        "--output-type", "json",
-					    })
-					end
-					
-					table.insert(file_status_args, path)
-					
-					local file_status = vim.system(
-					    file_status_args,
-					    { stdout = false, stderr = false }
-					):wait()
+					local file_status = vim.system({
+					    "sops",
+					    "filestatus",
+					    "--input-type", "json",
+					    "--output-type", "json",
+					    path,
+					}, {
+					    stdout = false,
+					    stderr = false,
+					}):wait()
 					if file_status.code == 0 then
 						vim.b[bufnr].sops = "e"
 						return true
